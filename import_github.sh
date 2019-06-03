b453615628cbb18dcf49d781851216808ffe2bc6
Rowix4 <Rowix4@gmail.com>
2019-06-03 16:48:26 +0200

    edit date

--- import_github.sh
+++ import_github.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -f Downloads/2015-01-01-15.json.gz/2015-01-01-15.json
-rm -f Downloads/2015-01-01-15.json.gz/2015-01-01-15.json.gz
-rm -r -f Downloads/2015-01-01-15.json.gz/
+rm -f Downloads/2018-09-19-15.json.gz/2018-09-19-15.json
+rm -f Downloads/2018-09-19-15.json.gz/2018-09-19-15.json.gz
+rm -r -f Downloads/2018-09-19-15.json.gz/
 rm -r -f Downloads
 mkdir Downloads
-wget https://data.gharchive.org/2015-01-01-15.json.gz -P Downloads/2015-01-01-15.json.gz
-gunzip Downloads/2015-01-01-15.json.gz/2015-01-01-15.json.gz
+wget https://data.gharchive.org/2018-09-19-15.json.gz -P Downloads/2018-09-19-15.json.gz
+gunzip Downloads/2018-09-19-15.json.gz/2018-09-19-15.json.gz
