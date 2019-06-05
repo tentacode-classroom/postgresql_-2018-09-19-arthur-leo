8f6529dc53c6b8b06ba99936e9f58bbb0c819a69
arthur <arthur.genthial@ynov.com>
2019-06-05 12:04:59 +0200

    edit

--- import_github.sh
+++ import_github.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-rm -f Downloads/2018-09-19-15.json.gz/2018-09-19-15.json
-rm -f Downloads/2018-09-19-15.json.gz/2018-09-19-15.json.gz
-rm -r -f Downloads/2018-09-19-15.json.gz/
 rm -r -f Downloads
 mkdir Downloads
 wget https://data.gharchive.org/2018-09-19-15.json.gz -P Downloads/2018-09-19-15.json.gz
--- scripts/create_super_user.sh
+++ scripts/create_super_user.sh
@@ -1,2 +1,3 @@
 CREATE ROLE super_admin LOGIN PASSWORD '12345' SUPERUSER;
 CREATE DATABASE super_admin OWNER super_admin;
+sudo service postgresql restart
